1f59b63db556137571be4f9236a5fd5accfcfe2d
KwakesProject <kwakes@me.com>
2016-09-28 11:15:06 +0100

    update to webpack to get hot reloader

--- webpack.config.js
+++ webpack.config.js
@@ -8,19 +8,25 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
       }
-    }]
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: './'
+    contentBase: './',
+    inline: true,
+    port: 8080
   }
 };
